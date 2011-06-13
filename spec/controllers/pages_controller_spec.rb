c81630d4ef745796f1c1c5e9e4d15e316e0fbac2
Sean Samenfeld-Specht <seaness81@gmail.com>
2011-06-12 22:40:38 -0700

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -56,4 +56,11 @@ describe PagesController do
                                     :content => @base_title + " | Help")
     end
   end
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
 end
